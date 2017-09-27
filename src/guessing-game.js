0968083d023dd4c9e7f2958ce2c8f5629eff5217
ManGysITI <mangysiti@gmail.com>
2017-09-28 01:46:27 +0300

    Methods implemented: guess, lower, greater

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+         this.min       = 0;
+         this.max       = 0 ;
+         this.middle    = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if (this.min <= this.max) {
+            this.middle = Math.round((this.min + this.max) / 2);
+            console.log(" min=" + this.min + " middle=" + this.middle + " max=" + this.max);
+        }
+        return this.middle;
     }
 
     lower() {
-
+        this.max =  this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
